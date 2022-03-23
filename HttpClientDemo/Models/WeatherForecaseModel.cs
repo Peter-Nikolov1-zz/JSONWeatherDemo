70f8d1dea383fef082f9cc50bd31c9bb81e95242
Peter-Nikolov1 <nikolov_peter@yahoo.com>
2022-03-22 20:51:16 -0400

    Demo Weather API

---
+++ HttpClientDemo/Models/WeatherForecaseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HttpClientDemo.Models
+{
+    public class WeatherForecastModel
+    {
+        public DayForecastModel[] Consolidated_weather { get; set; }
+        public DateTime Sun_rise { get; set; }
+        public DateTime Sun_set { get; set; }
+        public string Title { get; set; }
+        public string Timezone { get; set; }
+    }
+}
